5d63d9dcd0ec861f4f34531c78b00c8babb63aad
Micheal Waldrup <waldrupm@gmail.com>
2021-10-06 16:29:25 -0400

    merchant dashboard - links

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -7,4 +7,13 @@ describe 'Merchant dashboard' do
     visit "/merchants/#{merchant.id}/dashboard"
     expect(page).to have_content(merchant.name)
   end
+
+  it 'shows links to merchant items and invoices' do
+    merchant = Merchant.create(name: "Gary's Tavern")
+
+    visit "/merchants/#{merchant.id}/dashboard"
+
+    expect(page).to have_link('My Items')
+    expect(page).to have_link('My Invoices')
+  end
 end
